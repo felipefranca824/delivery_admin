8275c88e6cdde7f63ac29d0a817e44135aa77c65
Felipe França <felipefranca824@gmail.com>
2023-05-10 14:01:41 -0300

    disable payment type

--- lib/src/modules/payment_type/widgets/payment_type_item.dart
+++ lib/src/modules/payment_type/widgets/payment_type_item.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
+import '../../../core/ui/styles/colors_app.dart';
 import '../../../core/ui/styles/text_styles.dart';
 import '../../../models/payment_type_model.dart';
 
@@ -13,6 +14,7 @@ class PaymentTypeItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colorAll = paymentTypeModel.enabled ? Colors.black : Colors.grey;
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
@@ -27,8 +29,10 @@ class PaymentTypeItem extends StatelessWidget {
               errorBuilder: (context, error, stackTrace) {
                 return Image.asset(
                   'assets/images/icons/payment_notfound_icon.png',
+                  color: colorAll,
                 );
               },
+              color: colorAll,
             ),
             const SizedBox(
               width: 20,
@@ -40,14 +44,18 @@ class PaymentTypeItem extends StatelessWidget {
               children: [
                 Text(
                   'Forma de Pagamento',
-                  style: context.textStyles.textRegular,
+                  style: context.textStyles.textRegular.copyWith(
+                    color: colorAll,
+                  ),
                 ),
                 const SizedBox(
                   height: 10,
                 ),
                 Text(
                   paymentTypeModel.name,
-                  style: context.textStyles.textTitle,
+                  style: context.textStyles.textTitle.copyWith(
+                    color: colorAll,
+                  ),
                 ),
               ],
             ),
@@ -56,7 +64,14 @@ class PaymentTypeItem extends StatelessWidget {
                 alignment: Alignment.bottomRight,
                 child: TextButton(
                   onPressed: () {},
-                  child: const Text('Editar'),
+                  child: Text(
+                    'Editar',
+                    style: context.textStyles.textMedium.copyWith(
+                      color: paymentTypeModel.enabled
+                          ? context.colors.primary
+                          : Colors.grey,
+                    ),
+                  ),
                 ),
               ),
             )
