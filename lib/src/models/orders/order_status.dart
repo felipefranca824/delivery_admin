78f493bcebb76299e5a986e3b3008000006c8d0d
Felipe França <felipefranca824@gmail.com>
2023-05-13 19:49:39 -0300

    implemented screen order

---
+++ lib/src/models/orders/order_status.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+enum OrderStatus {
+  pending('Pendente', 'P', Colors.blue),
+  confirmed('Confirmado', 'C', Colors.green),
+  cancel('Cancelado', 'R', Colors.blue),
+  finalized('Finalizado', 'D', Colors.black);
+
+  final String name;
+  final String acronym;
+  final Color color;
+
+  const OrderStatus(this.name, this.acronym, this.color);
+
+  static OrderStatus parse(String acronym) {
+    return values.firstWhere((element) => element.acronym == acronym);
+  }
+}
---
+++ lib/src/modules/order/order_controller.dart
@@ -0,0 +1,55 @@
+import 'dart:developer';
+
+import 'package:mobx/mobx.dart';
+
+import '../../models/orders/order_model.dart';
+import '../../models/orders/order_status.dart';
+import '../../repositories/order/order_repository.dart';
+part 'order_controller.g.dart';
+
+enum OrderStateStatus {
+  initial,
+  loading,
+  loaded,
+  error,
+}
+
+class OrderController = OrderControllerBase with _$OrderController;
+
+abstract class OrderControllerBase with Store {
+  final OrderRepository _orderRepository;
+
+  @readonly
+  var _status = OrderStateStatus.initial;
+
+  @readonly
+  var _orders = <OrderModel>[];
+
+  @readonly
+  String? _errorMessage;
+
+  late final DateTime _today;
+
+  @readonly
+  OrderStatus? _statusFilter;
+
+  OrderControllerBase(this._orderRepository) {
+    final todayNow = DateTime.now();
+    _today = DateTime(todayNow.year, todayNow.month, todayNow.day);
+  }
+
+  @action
+  Future<void> findOrders() async {
+    try {
+      _status = OrderStateStatus.loading;
+
+      _orders = await _orderRepository.findAllOrder(_today, _statusFilter);
+
+      _status = OrderStateStatus.loaded;
+    } catch (e, s) {
+      log('Erro ao buscar pedidos do dia', error: e, stackTrace: s);
+      _status = OrderStateStatus.error;
+      _errorMessage = 'Erro ao buscar pedidos do dia';
+    }
+  }
+}
